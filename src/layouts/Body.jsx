8421b56767ddbfc7e063db13adb02987f67ad24d
Chow Jia Ying <chowjiaying211@gmail.com>
2021-01-05 17:13:23 +0800

    Add a container to the app

--- src/layouts/Body.jsx
+++ src/layouts/Body.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Print } from '../screens';
+import { Container } from "@material-ui/core";
 
 export default function Body() {
   return (
-    <div>
+    <Container>
       <Print />
-    </div>
+    </Container>
   )
 }
 
