18818e9924f9f165860d902fff4eba1a040e85c5
Chow Jia Ying <chowjiaying211@gmail.com>
2021-01-05 22:21:58 +0800

    Create a dropdown

--- src/screens/Print.jsx
+++ src/screens/Print.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-import { Button, Typography } from '@material-ui/core';
+import { Button, FormControl, InputLabel, MenuItem, Select, Typography } from '@material-ui/core';
 import { Add } from '@material-ui/icons';
 
 export default function Print() {
+  const printerList = ['psc008', 'psc011'];
+  const [selectedPrinter, setSelectedPrinter] = React.useState(printerList[0]);
+  const handleChangePrinter = (event) => setSelectedPrinter(event.target.value);
   return (
     <>
       <Typography variant="h1">Print a document</Typography>
@@ -14,6 +17,15 @@ export default function Print() {
       >
         Upload file
       </Button>
+      <FormControl>
+        <InputLabel>Choose Printer</InputLabel>
+        <Select
+          value={selectedPrinter}
+          onChange={handleChangePrinter}
+        >
+          {printerList.map(printer => <MenuItem value={printer}>{printer}</MenuItem>)}
+        </Select>
+      </FormControl>
     </>
   )
 }
