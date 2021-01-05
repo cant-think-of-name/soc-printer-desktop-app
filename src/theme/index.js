e3a616c1bd1d9ffa39b75e5a059779832829796e
Chow Jia Ying <chowjiaying211@gmail.com>
2021-01-05 21:33:00 +0800

    Add typography theme

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,8 +1,10 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 import palette from "./palette";
+import typography from "./typography";
 
 export default createMuiTheme({
   ...palette,
+  ...typography,
   overrides: {
     MuiPaper: {
       root: {
---
+++ src/theme/typography.js
@@ -0,0 +1,10 @@
+export default {
+  typography: {
+    h1: {
+      fontSize: '1.25rem',
+      lineHeight: '1.875rem',
+      letterSpacing: '0.15px',
+      fontWeight: 400,
+    },
+  },
+}
